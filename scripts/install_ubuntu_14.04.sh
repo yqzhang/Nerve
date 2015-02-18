e60f314e83fc9441be6f9aff2d2466292fd05eca
Yunqi Zhang <zhangyunqi.cs@gmail.com>
2015-02-18 17:20:10 -0500

    sudo permission

--- scripts/install_ubuntu_14.04.sh
+++ scripts/install_ubuntu_14.04.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y wget tar
 wget http://sourceforge.net/projects/perfmon2/files/libpfm4/libpfm-4.6.0.tar.gz
 
 tar -xvf libpfm-4.6.0.tar.gz
-cd libpfm-4.6.0 && make && make install && cd ..
+cd libpfm-4.6.0 && make && sudo make install && cd ..
 rm -rf libpfm-4.6.0 libpfm-4.6.0.tar.gz
 
 cd ../src/ && make
