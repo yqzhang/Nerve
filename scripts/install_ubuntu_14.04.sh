a945af3ab12faf4ba78af62229e52844ab2735f4
yqzhang <zhangyunqi.cs@gmail.com>
2015-02-18 16:47:38 -0500

    add travis-ci

--- scripts/install_ubuntu_14.04.sh
+++ scripts/install_ubuntu_14.04.sh
@@ -2,21 +2,7 @@
 
 set -ex
 
-hash wget 2>/dev/null || {
-  echo >&2 "$0: [ERROR] wget is not installed. Aborting."
-  exit 1
-}
-
-hash tar 2>/dev/null || {
-  echo >&2 "$0: [ERROR] tar is not installed. Aborting."
-  exit 1
-}
-
-if [ "$EUID" -ne 0 ]
-then
-  echo >&2 "$0: [ERROR] Sudo access required. Aborting."
-  exit 1
-fi
+apt-get install -y wget tar
 
 # Get libpfm 4.6.0
 wget http://sourceforge.net/projects/perfmon2/files/libpfm4/libpfm-4.6.0.tar.gz
@@ -24,3 +10,5 @@ wget http://sourceforge.net/projects/perfmon2/files/libpfm4/libpfm-4.6.0.tar.gz
 tar -xvf libpfm-4.6.0.tar.gz
 cd libpfm-4.6.0 && make && make install && cd ..
 rm -rf libpfm-4.6.0 libpfm-4.6.0.tar.gz
+
+cd src/ && make
