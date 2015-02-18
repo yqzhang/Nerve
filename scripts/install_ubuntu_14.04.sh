3f4280475d2a9c4a85d3be007c7ad34a9e2c6283
Yunqi Zhang <zhangyunqi.cs@gmail.com>
2015-02-18 16:56:36 -0500

    oops

--- scripts/install_ubuntu_14.04.sh
+++ scripts/install_ubuntu_14.04.sh
@@ -2,7 +2,13 @@
 
 set -ex
 
-apt-get install -y wget tar
+if [ "$EUID" -ne 0 ]
+then
+  echo >&2 "$0: [ERROR] Sudo access required. Aborting."
+  exit 1
+fi
+
+sudo apt-get install -y wget tar
 
 # Get libpfm 4.6.0
 wget http://sourceforge.net/projects/perfmon2/files/libpfm4/libpfm-4.6.0.tar.gz
