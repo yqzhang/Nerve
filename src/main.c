16ac4619907caeb43b2b8e2999313de0d4ecf2a3
yqzhang <zhangyunqi.cs@gmail.com>
2015-02-19 18:10:11 -0500

    get rid of the hard tabs

--- src/main.c
+++ src/main.c
@@ -21,10 +21,10 @@
 #include "proc_sample.h"
 
 typedef struct {
-	const char *events[MAX_GROUPS];
-	int num_groups;
-	int print;
-	int pin;
+  const char *events[MAX_GROUPS];
+  int num_groups;
+  int print;
+  int pin;
 } options_t;
 
 options_t options;
@@ -36,12 +36,13 @@ static void sig_handler(int n) {
 }
 
 static void usage(void) {
-	printf("usage: task [-h] [-g] [-p] [-P] [-e event1,event2,...] cmd\n"
-		"-h\t\tget help\n"
-		"-p\t\tprint counts every second\n"
-		"-P\t\tpin events\n"
-		"-e ev,ev\tgroup of events to measure (multiple -e switches are allowed)\n"
-		);
+  printf(
+      "usage: task [-h] [-g] [-p] [-P] [-e event1,event2,...] cmd\n"
+      "-h\t\tget help\n"
+      "-p\t\tprint counts every second\n"
+      "-P\t\tpin events\n"
+      "-e ev,ev\tgroup of events to measure (multiple -e switches are "
+      "allowed)\n");
 }
 
 int main(int argc, char **argv) {
@@ -50,34 +51,33 @@ int main(int argc, char **argv) {
   setlocale(LC_ALL, "");
 
   while ((c = getopt(argc, argv, "+he:pP:")) != -1) {
-    switch(c) {
-			case 'e':
-				if (options.num_groups < MAX_GROUPS) {
-					options.events[options.num_groups++] = optarg;
-				} else {
-					errx(1, "you cannot specify more than %d groups.\n",
-						MAX_GROUPS);
-				}
-				break;
-			case 'p':
-				options.print = 1;
-				break;
-			case 'P':
-				options.pin = 1;
-				break;
-			case 'h':
-				usage();
-				exit(0);
-			default:
-				errx(1, "unknown error");
+    switch (c) {
+      case 'e':
+        if (options.num_groups < MAX_GROUPS) {
+          options.events[options.num_groups++] = optarg;
+        } else {
+          errx(1, "you cannot specify more than %d groups.\n", MAX_GROUPS);
+        }
+        break;
+      case 'p':
+        options.print = 1;
+        break;
+      case 'P':
+        options.pin = 1;
+        break;
+      case 'h':
+        usage();
+        exit(0);
+      default:
+        errx(1, "unknown error");
     }
   }
-	if (options.num_groups == 0) {
-		options.events[0] = "cycles,instructions";
-		options.num_groups = 1;
-	}
+  if (options.num_groups == 0) {
+    options.events[0] = "cycles,instructions";
+    options.num_groups = 1;
+  }
 
-	signal(SIGINT, sig_handler);
+  signal(SIGINT, sig_handler);
 
   while (true) {
     // TODO: take a snapshot of the running processes
