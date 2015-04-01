679177051f270469a9293a774a11713fb37f556f
yqzhang <zhangyunqi.cs@gmail.com>
2015-04-01 15:37:43 -0400

    [neuron] fix the build

--- scripts/install_ubuntu_14.04.sh
+++ scripts/install_ubuntu_14.04.sh
@@ -14,7 +14,7 @@ rm -rf libpfm-4.6.0 libpfm-4.6.0.tar.gz
 # Get Jansson
 wget http://www.digip.org/jansson/releases/jansson-2.7.tar.gz
 tar -xvf jansson-2.7.tar.gz
-cd jasson-2.7 && make && sudo make install && cd ..
+cd jansson-2.7 && make && sudo make install && cd ..
 rm -rf jansson-2.7 jansson-2.7.tar.gz
 
 cd src/ && make
