8b8664f28ecca7e7e93eb878318e2dcd5e252163
yqzhang <zhangyunqi.cs@gmail.com>
2015-04-02 16:55:09 -0400

    [neuron] add the snooping protocol

--- src/app_sample.h
+++ src/app_sample.h
@@ -11,4 +11,44 @@
 #ifndef __APP_SAMPLE_H__
 #define __APP_SAMPLE_H__
 
+#include <sys/types.h>
+
+#define MAX_HOSTNAME_LENGTH 32
+#define MAX_NUM_APPLICATIONS 8
+
+typedef struct application {
+  char hostname[MAX_HOSTNAME_LENGTH];
+  unsigned int port;
+} application_t;
+
+typedef struct application_list {
+  application_t applications[MAX_NUM_APPLICATIONS];
+  size_t size;
+} application_list_t;
+
+// All possible commands that can be sent as snoop request
+typedef enum {
+  SNOOP_CMD_RESET = 0x00,
+  SNOOP_CMD_PERF = 0x01,
+} snoop_command;
+
+typedef struct snoop_request {
+  short snoop_command;
+} snoop_request_t;
+
+// Possible return code of a snoop request
+typedef enum {
+  SNOOP_REPLY_SUCCESS = 0x00,
+  SNOOP_REPLY_ERROR = 0x01,
+} snoop_reply_code;
+
+typedef struct snoop_reply {
+  // return code
+  short reply_code;
+  // total number of requests that have been sent
+  unsigned long num_of_reuqests;
+  // tail latency of the sent requests microseconds
+  double tail_latency;
+} snoop_reply_t;
+
 #endif
