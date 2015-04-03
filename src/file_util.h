5727b23696f39770667f6c9bfcc6b1851e9fce6e
yqzhang <zhangyunqi.cs@gmail.com>
2015-04-03 16:33:42 -0400

    [neuron] initial checkin for file util

---
+++ src/file_util.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (c) 2015, University of Michigan.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#ifndef __FILE_UTIL__
+#define __FILE_UTIL__
+
+void read_file();
+
+void write_file();
+
+#endif
