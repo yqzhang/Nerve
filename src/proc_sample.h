ac97d90ec3485150ab0fcf9c09434499191d6805
ramsrivatsa <ramsrivats@gmail.com>
2015-04-15 02:25:23 -0400

    implemented changes to be made :- 1. delta 2. int to unsigned long

--- src/proc_sample.h
+++ src/proc_sample.h
@@ -21,7 +21,6 @@ typedef struct process {
   unsigned long cminor_fault;
   unsigned long major_fault;
   unsigned long cmajor_fault;
-  unsigned long context_switches;
   unsigned long long total_fault;
   unsigned long utime;
   unsigned long stime;
@@ -29,6 +28,9 @@ typedef struct process {
   unsigned long cstime;
   unsigned long ttime;
   unsigned long long cpu_affinity;
+  int io_read;
+  int io_write;
+  int context_switches;
   float cpu_utilization;
   float virtual_mem_utilization;
   float real_mem_utilization;
